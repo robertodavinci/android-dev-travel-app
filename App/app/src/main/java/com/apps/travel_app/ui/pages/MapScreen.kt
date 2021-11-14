a8904599c12cf64abbbc2d8e47a180da178f3c7c
Vincenzo Manto <vincenzo.manto@sys-datgroup.com>
2021-11-14 17:03:14 +0100

    Map hand drawing

--- App/app/src/main/java/com/apps/travel_app/ui/pages/MapScreen.kt
+++ App/app/src/main/java/com/apps/travel_app/ui/pages/MapScreen.kt
@@ -1,34 +1,39 @@
 package com.apps.travel_app.ui.pages
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Point
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.input.pointer.PointerInputChange
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.apps.travel_app.R
-import com.apps.travel_app.ui.theme.cardPadding
-import com.apps.travel_app.ui.theme.textHeading
-import com.apps.travel_app.ui.theme.textLightColor
+import com.apps.travel_app.ui.theme.*
 import com.apps.travel_app.ui.utils.rememberMapViewWithLifecycle
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.google.android.libraries.maps.CameraUpdateFactory
-import com.google.android.libraries.maps.model.LatLng
-import com.google.android.libraries.maps.model.MapStyleOptions
-import com.google.android.libraries.maps.model.MarkerOptions
-import com.google.android.libraries.maps.model.PolylineOptions
+import com.google.android.libraries.maps.GoogleMap
+import com.google.android.libraries.maps.model.*
 import com.google.maps.android.ktx.awaitMap
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 
 @Composable
@@ -40,40 +45,82 @@ fun MapScreen(context: Context) {
     )
 
     val mapView = rememberMapViewWithLifecycle()
+    val center = LatLng(44.0, 10.0)
+    var polygonOpt = PolygonOptions()
+    var map: GoogleMap? = null
 
     Box(modifier = Modifier.fillMaxSize()) {
 
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.White)
+                .background(White)
                 .wrapContentSize(Alignment.Center)
+                .pointerInput(Unit) {
+                    detectDragGestures(
+                        onDragStart = { position ->
+                            if (map != null) {
+                                map!!.clear()
+                                polygonOpt = PolygonOptions()
+                                polygonOpt.add(screenCoordinatesToLatLng(position, map))
+                                polygonOpt.strokeColor(Color.BLUE)
+                                map!!.addPolygon(polygonOpt)
+                            }
+                        },
+                        onDrag = { event, _ -> mapDrag(map, event, polygonOpt) },
+                        onDragEnd = {
+                            if (map != null) {
+                                val points = polygonOpt.points
+                                var x = 0.0
+                                var y = 0.0
+                                val pointCount: Int = points.size
+                                for (i in 0 until pointCount - 1) {
+                                    val point = points[i]
+                                    x += point.latitude
+                                    y += point.longitude
+                                }
+                                x /= pointCount
+                                y /= pointCount
+                                val circle = CircleOptions()
+                                circle.strokeColor(Color.BLUE)
+                                circle.center(center)
+                                circle.radius(
+                                    sqrt(
+                                        (points[0].latitude - x).pow(2.0) + (points[0].longitude - y).pow(
+                                            2.0
+                                        )
+                                    )
+                                )
+                                map!!.clear()
+                                map!!.addCircle(circle)
+                            }
+                        }
+                    )
+                },
         ) {
             AndroidView({ mapView }) { mapView ->
                 CoroutineScope(Dispatchers.Main).launch {
-                    val map = mapView.awaitMap()
-                    map.uiSettings.isZoomControlsEnabled = false
+                    map = mapView.awaitMap()
 
-                    map.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.mapstyle))
+                    val completeMap = map!!
+                    completeMap.uiSettings.isScrollGesturesEnabled = false
+                    completeMap.uiSettings.isZoomControlsEnabled = false
 
-                    map.uiSettings.isMapToolbarEnabled = false;
-
-                    val pickUp = LatLng(45.0, 11.0)
-                    val destination = LatLng(44.0, 10.0)
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(destination, 6f))
-                    val markerOptions = MarkerOptions()
-                        .title("Sydney Opera House")
-                        .position(pickUp)
-                    map.addMarker(markerOptions)
-
-                    val markerOptionsDestination = MarkerOptions()
-                        .title("Restaurant Hubert")
-                        .position(destination)
-                    map.addMarker(markerOptionsDestination)
-
-                    map.addPolyline(
-                        PolylineOptions().add(pickUp, destination)
+                    completeMap.setMapStyle(
+                        MapStyleOptions.loadRawResourceStyle(
+                            context,
+                            R.raw.mapstyle
+                        )
                     )
+
+                    completeMap.uiSettings.isMapToolbarEnabled = false;
+
+                    completeMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 6f))
+
+                    val markerOptionscenter = MarkerOptions()
+                        .title("Restaurant Hubert")
+                        .position(center)
+                    completeMap.addMarker(markerOptionscenter)
 
                 }
 
@@ -87,27 +134,50 @@ fun MapScreen(context: Context) {
                 .fillMaxWidth()
                 .align(Alignment.TopCenter)
                 .height(200.dp)
-                .background(brush = Brush.verticalGradient(
-                    colors = listOf(
-                        textLightColor,
-                        Color.Transparent
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            textLightColor,
+                            Transparent
+                        )
                     )
-                ))
+                )
         )
 
         Text(
             text = "Spin & pin",
-            color = Color.White,
+            color = White,
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
             fontSize = textHeading,
-            modifier = Modifier.fillMaxWidth().align(Alignment.TopCenter).padding(cardPadding)
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+                .padding(cardPadding)
         )
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun MapScreenPreview() {
-    //MapScreen()
+fun screenCoordinatesToLatLng(position: Offset, map: GoogleMap?): LatLng? {
+    if (map == null)
+        return null
+    val x = position.x.roundToInt() // get screen x position or coordinate
+    val y = position.y.roundToInt() // get screen y position or coordinate
+    val point = Point(x, y) // accept int x,y value
+    return map.projection.fromScreenLocation(point) // convert x,y to LatLng
+}
+
+fun mapDrag(map: GoogleMap?, motionEvent: PointerInputChange, polygonOpt: PolygonOptions) {
+    if (map == null)
+        return
+
+    val latLng = screenCoordinatesToLatLng(motionEvent.position, map) ?: return
+
+    if (!polygonOpt.points.none { point -> point.equals(latLng) })
+        return
+
+    map.clear()
+    polygonOpt.add(latLng)
+
+    map.addPolygon(polygonOpt)
 }
